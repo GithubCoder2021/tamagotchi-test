c898c391eeaa1c6de6a3526c3f9c3a2c8c50e2d1
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2022-10-27 13:05:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 var hunger = 0;
 var mood = 0;
 var age =  0;
+document.getElementById('hungerBut').style.visibility = 'hidden';
+document.getElementById('happyBut').style.visibility = 'hidden';
 
 // hidden variables
 var timeTillEvo = 65*60;
+while (timeTillEvo > 0) {
+ timeTillEvo--;
+}
 
 
 function hatchTama() {
@@ -13,6 +18,8 @@ function hatchTama() {
  document.getElementById("happy").innerText= mood;
  document.getElementById("tamaAge").innerText= age;
  document.getElementById('hatch').style.visibility = 'hidden';
+ document.getElementById('hungerBut').style.visibility = 'visible';
+ document.getElementById('happyBut').style.visibility = 'visible';
   }
 
 function feedTama() {
