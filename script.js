752e5dc95a2a88c4b4f871026d1518e4b5add069
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2022-10-27 07:36:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var hunger = 4;
 var mood = 4;
+
+function hatchTama() {
+document.getElementById("tama").src="baby.gif";
+ document.getElementById("stomach")= hunger;
+ document.getElementById("happy")= mood;
+  }
